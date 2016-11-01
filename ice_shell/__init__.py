9a3632f3974a4888431ba45195cc9e114a6f6a18
George Lestaris <glestaris@gmail.com>
2016-11-01 20:14:10 +0000

    Move config paths to shell

--- ice_shell/__init__.py
+++ ice_shell/__init__.py
@@ -1,2 +1,20 @@
 """iCE Shell module."""
+import os
+import sys
 from .shell import Shell, CfgShell
+
+#
+# Configuration directories list
+#
+
+CONFIG_DIRS = [
+    os.path.join(sys.prefix, 'etc', 'ice'),
+    os.path.join(sys.prefix, 'local', 'etc', 'ice'),
+    os.path.join('/etc/ice'),
+    os.path.expanduser('~/.ice')
+]
+env_paths_str = os.environ.get('ICE_CONFIG_PATHS')
+if env_paths_str is not None:
+    env_paths = env_paths_str.split(':')
+    for path in env_paths:
+        CONFIG_DIRS.append(os.path.abspath(path))
